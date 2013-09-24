ce195a83672182e27bbca10f331888cf3feb807a
michaelmckisson <mikem13@dhcp-10-142-215-188.uawifi.arizona.edu>
2013-09-24 11:31:39 -0700

    Online

--- index.php
+++ index.php
@@ -1,17 +1,31 @@
-<!-- Let's include the header file that we created above -->
-<?php include('includes/header.php'); ?> 
+<head> 
+	<title>My Page</title> 
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
+	<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
+</head>
 <body>
  <div data-role="page">
  
-   <header data-role="header">
-
+   <header data-role="header" data-theme="b">
+<h2>TV Mobile</h2>
    </header>
    <div data-role="content">
-
+	<ul data-role="listview" data-inset="true" data-filter="true" data-theme="b">
+		<li><a href="site.php?siteName=nettuts">All Posts</a></li>
+		<li><a href="site.php?siteName=tucvelnews">News</a></li>
+		<li><a href="site.php?siteName=blog">Blog</a></li>
+		<li><a href="site.php?siteName=photos">Photos</a></li>
+		<li><a href="site.php?siteName=rides">Rides &amp; Events</a></li>
+		<li><a href="site.php?siteName=stolen">Stolen Bikes</a></li>
+		<li><a href="site.php?siteName=tips">News Tips</a></li>
+		
+	</ul>
    </div>
 
-   <footer data-role="footer">
-
+   <footer data-role="footer" data-theme="b">
+&#169; Tucson Velo
    </footer>
 
  </div>
