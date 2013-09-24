e04815a08f8883bd46e2be17628b8827b692df90
michaelmckisson <mikem13@dhcp-10-142-215-188.uawifi.arizona.edu>
2013-09-24 11:25:13 -0700

    Current as 9-24

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html> 
+<html> 
+   <head> 
+   <meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+   <title> Tucson Velo Mobile </title> 
+
+   <link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a1/jquery.mobile-1.0a1.min.css" />
+   <link rel="apple-touch-icon" href="tvlogo.png" />
+
+   <script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+    <script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
+</head>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!-- Let's include the header file that we created above -->
+<?php include('includes/header.php'); ?> 
+<body>
+ <div data-role="page">
+ 
+   <header data-role="header">
+
+   </header>
+   <div data-role="content">
+
+   </div>
+
+   <footer data-role="footer">
+
+   </footer>
+
+ </div>
+ 
+</body>
+</html>
